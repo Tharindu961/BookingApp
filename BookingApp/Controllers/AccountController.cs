f24ad5a5984440b2978a3d72aa65e26017804478
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2023-04-29 20:00:55 +0530

    Add project files.

---
+++ BookingApp/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using BookingApp.Models;
+
+namespace BookingApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=BookingApp;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+        }
+
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from Admin where AdminName='"+acc.Name+"' and Password'"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BookingApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookingAppContext _context = new BookingAppContext();
+        public ActionResult Index()
+        {
+            var listodData = _context.Goods.ToList();
+            return View(listodData);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Good model)
+        {
+            _context.Goods.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _context.Goods.Where(x => x.GoodsId == id).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Good Model)
+        {
+            var data = _context.Goods.Where(x => x.GoodsId == Model.GoodsId).FirstOrDefault();
+            if(data != null)
+            {
+                data.FromAdderess = Model.FromAdderess;
+                data.ToAddress = Model.ToAddress;
+                data.GoodsType = Model.GoodsType;
+                data.Date = Model.Date;
+                data.Weight = Model.Weight;
+                data.Price = Model.Price;
+            }
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var data = _context.Goods.Where(x => x.GoodsId == id).FirstOrDefault();
+            return View(data);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var data = _context.Goods.Where(x => x.GoodsId == id).FirstOrDefault();
+            _context.Goods.Remove(data);
+            _context.SaveChanges();
+            ViewBag.Message = "Record Delete Sucessfully";
+            return RedirectToAction("index");
+        }
+    }
+}
